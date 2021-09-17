fdb8fb312b3bcc531ac7cfc3b2c4c9ed7d2982b3
muhammed-topgul <muhammedtopgul.29@gmail.com>
2021-09-17 16:27:40 +0300

    2. Domain Model - set schema to entity usage

--- 02-domain-model/src/main/java/com/muhammedtopgul/hibernatedocs/entity/Book.java
+++ 02-domain-model/src/main/java/com/muhammedtopgul/hibernatedocs/entity/Book.java
@@ -7,12 +7,14 @@ import lombok.Setter;
 
 import javax.persistence.Embedded;
 import javax.persistence.Entity;
+import javax.persistence.Table;
 
 /**
  * created by Muhammed Topgul on 17/09/2021 at 14:45
  */
 
 @Entity(name = "Book")
+@Table(catalog = "public", schema = "custom_schema", name = "book")
 @Getter
 @Setter
 public class Book extends BaseId {
