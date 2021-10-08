4b24b914218949533b79c9f0884af7a8daada6d2
muhammed-topgul <muhammedtopgul.29@gmail.com>
2021-10-08 08:46:38 +0300

    1. Domain Model - Single Table inheritance

--- 01-domain-model/07-inheritance/src/main/java/com/muhammedtopgul/hibernatedocs/inheritace/entity/Account.java
+++ 01-domain-model/07-inheritance/src/main/java/com/muhammedtopgul/hibernatedocs/inheritace/entity/Account.java
@@ -2,16 +2,24 @@ package com.muhammedtopgul.hibernatedocs.inheritace.entity;
 
 import com.muhammedtopgul.hibernatedocs.commons.BaseId;
 import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
-import javax.persistence.MappedSuperclass;
+import javax.persistence.*;
 import java.math.BigDecimal;
 
 /**
  * created by Muhammed Topgul on 07/10/2021 at 12:55
  */
 
-@MappedSuperclass
-@Data
+// 1. strategy: @MappedSuperclass
+// @MappedSuperclass
+// 2. strategy: InheritanceType.SINGLE_TABLE
+@Entity(name = "Account")
+@Table(schema = "inheritances", name = "account")
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
+@Getter
+@Setter
 public abstract class Account extends BaseId {
 
     private String owner;
